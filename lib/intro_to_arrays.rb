fc7198835892bc31ec38259fba00841c38f4998b
Patricia Carter <patricia4194@gmail.com>
2018-08-07 15:14:51 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -22,6 +22,14 @@ def last_element(array)
   array[-1]
 end 
 
+def first_element_with_array_methods
+  array.first
+end 
+
+def last_element_with_array_methods
+  array.last 
+end
+
 def length_of_array(array)
   array.length
 end 
